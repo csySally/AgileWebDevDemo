4d7d19870eda7836aa994337c7b15ffaa7d90adb
Sally Chen <23687599@student.uwa.edu.au>
2024-04-26 21:00:34 +0800

    login and register done

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,8 +1,17 @@
 # app/__init__.py
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
-from app import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+login = LoginManager(app)
+login.login_view = "login"  # 'login' 是登录视图函数的端点名称
+
+from app import routes, models
